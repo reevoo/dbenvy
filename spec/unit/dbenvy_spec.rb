19aad1b636736fd03384691537bbc47e97a171f1
Ed Robinson <ed.robinson@reevoo.com>
2014-12-10 11:28:09 +0000

    First Experiment

---
+++ spec/unit/dbenvy_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+require 'uri'
+require 'rack/utils'
+require 'yaml'
+
+class DBEnvy
+  def self.to_hash
+    new.to_hash
+  end
+
+  def self.yaml
+    new.yaml
+  end
+
+  attr_accessor :uri
+
+  def initialize
+    self.uri = URI.parse ENV['DATABASE_URL'] if ENV['DATABASE_URL']
+  end
+
+  def yaml
+    YAML.dump( { rails_env => to_hash } ) if uri && rails_env
+  end
+
+  def to_hash
+    credentials.merge options
+  end
+
+  private
+
+  def rails_env
+    ENV['RAILS_ENV']
+  end
+
+  def credentials
+    {
+      "adapter" => uri.scheme,
+      "username" => uri.user,
+      "password" => uri.password,
+      "host" => uri.host,
+      "port" => uri.port,
+      "database" => database,
+    }
+  end
+
+  def options
+    Rack::Utils.parse_nested_query uri.query
+  end
+
+  def database
+    uri.path.dup.tap { |p| p.slice!("/") }
+  end
+end
+
+describe DBEnvy do
+  describe '.to_hash' do
+    context 'with a DATABSE_URL in the environment' do
+
+      let(:hash) { described_class.to_hash }
+
+      before do
+        ENV['DATABASE_URL'] = 'mysql2://susan:sekret@127.0.0.1:3306/reevoo_live?encoding=utf8&view_options[awesome_database]=awesome_live'
+      end
+
+      it 'extracts the adapter' do
+        expect(hash['adapter']).to eq 'mysql2'
+      end
+
+      it 'extracts the host' do
+        expect(hash['host']).to eq '127.0.0.1'
+      end
+
+      it 'extracts the database name' do
+        expect(hash['database']).to eq 'reevoo_live'
+      end
+
+      it 'extracts the username' do
+        expect(hash['username']).to eq 'susan'
+      end
+
+      it 'extracts the password' do
+        expect(hash['password']).to eq 'sekret'
+      end
+
+      it 'extracts the port' do
+        expect(hash['port']).to eq 3306
+      end
+
+      it 'extracts some standard query params' do
+        expect(hash['encoding']).to eq 'utf8'
+      end
+    end
+  end
+
+  describe '.yaml' do
+    let(:yaml) { described_class.yaml }
+
+    context 'when the DATABASE_URL is not set' do
+
+      before do
+        ENV['RAILS_ENV'] = nil
+        ENV['DATABASE_URL'] = nil
+      end
+
+      it 'is nil' do
+        expect(yaml).to be_nil
+      end
+    end
+
+    context 'when RAILS_ENV is set' do
+      before do
+        ENV['RAILS_ENV'] = 'production'
+        ENV['DATABASE_URL'] = 'mysql2://reevoo:secret@db456.reevoover.com:3306/reevoo_awesome?encoding=utf8&view_options[awesome_database]=awesome_live&socket=%2Fvar%2Frun%2Fmysqld%2Fmysqld.sock'
+      end
+
+      it 'uses the current rails env and extracts the database info' do
+        expect(YAML.load(yaml)).to eq(
+          "production" => {
+            "adapter" => "mysql2",
+            "username" => "reevoo",
+            "port" => 3306,
+            "host" => "db456.reevoover.com",
+            "database" => "reevoo_awesome",
+            "password" => "secret",
+            "encoding" => "utf8",
+            "socket" => "/var/run/mysqld/mysqld.sock",
+            "view_options" => {"awesome_database" => "awesome_live"}})
+      end
+    end
+  end
+end
+
